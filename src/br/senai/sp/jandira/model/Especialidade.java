ff50a73009442e4620c5304d849e557c7f5e90f8
PAULA BLESA STANIUKAITIS <22282124@CFP127ALUNO.LOCAL>
2022-09-15 11:22:39 -0300

    parte 1

---
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.model;
+
+public class Especialidade {
+	
+	private String nome;
+	
+	// Método de acesso getters and setters
+	public void setNome(String novoNome) {
+		nome = novoNome;	
+	}
+	
+	public String getNome() {
+		return nome;
+		}
+	
+	
+}
